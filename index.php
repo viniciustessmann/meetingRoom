30c7c3bde432601e41335c4b121ec8639fe36ef6
Vinícius Schlee Tessmann <viniciusschleetessmann@gmail.com>
2017-08-19 13:39:03 -0300

    First commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+	<style type="text/css">
+		body{
+			background-color: green;
+		}
+
+		.container {
+			width: 100%;
+			height: 300px;
+			float: left;
+			background: orange;
+			margin-top: 10%;
+		}
+
+		.container p{
+			width: 100%;
+			margin: 0 auto;
+			text-align: center;
+			background: #FFF;
+			font-size: 124px;
+		}
+	</style>
+</head>
+<body>	
+	<div class="container">
+		<p>Livre</p>
+	</div>
+</body>
+</html>
